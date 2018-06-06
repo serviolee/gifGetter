1e53eaabd288d9b21efcd6a74dcbb15656af2a69
serviolee <serviolee@gmail.com>
2018-06-05 23:51:33 -0700

    implemented code to make it handle code, have two fields, store name and code inside localStorage, and search for a snippet by name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # gifGetter
-gifGetter - grabs gifs for your viewing pleasure
+gifGetter - app should take in code snippets and store them. When requested, the app should display code in a text area.
 
 Goals: 
 
@@ -7,10 +7,10 @@ Goals:
 - [x] click handler to capture data in text form field
 - [x] helper utilities to read and write to local storage
 
-- [ ] make it handle code! (use a textarea instead of a text)
-- [ ] have two fields, one is a short name the other is the snippet
-- [ ] goimg to store the name and code inside localStorage
+- [x] make it handle code! (use a textarea instead of a text)
+- [x] have two fields, one is a short name the other is the snippet
+- [x] going to store the name and code inside localStorage
 - [ ] display all snippets
-- [ ] search for a snippet using the name
+- [x] search for a snippet using the name
 
 
--- app.js
+++ app.js
@@ -1,22 +1,121 @@
 $(document).ready(function() {
 
+  var object = [];
+
   $('.setData').on('click', function() {
-    let textFieldValue = $('.textField').val();
-    $('.debug').text(textFieldValue);
-    localStorage.setItem('myFormTextData', textFieldValue);
+    var saveSnippet = function() {
+    	var tempObj = {};
+    	var tempArr = [];
+    	let snippetName = $('.textField').val();
+    	let snippet = $('.textSnippet').val();
+    	tempObj.name = snippetName;
+    	tempObj.code = snippet;
+    	tempArr.push(tempObj);
+        object.push(tempArr);
+    }
+    saveSnippet();
     $('.textField').val('');
-  })
+    $('.textSnippet').val('');
+    localStorage.setItem('storedSnippets', JSON.stringify(object));
+  });
 
   $('.getData').on('click', function(){
-  	let retrieveData = localStorage.getItem('myFormTextData');
-  	$('.debug').text(retrieveData);
-  })
+  	$('.snippet').remove();
+  	$('.notFound').remove();
+  	let search = $('.searchSnippets').val();
+  	if(!search) {
+  		$('.debug').append(`<div class='notFound'>Please input snippet</div>`);
+  	}
+  	let retrieveData = localStorage.getItem('storedSnippets');
+  	let obj = JSON.parse(retrieveData); // makes data iterable for searching
+  	let found = false;
+  	for (var i = 0; i < obj.length; i++) {
+  		let currentItem = obj[i][0];
+  		if (currentItem.name === search) {
+  			let codeSnippet = currentItem.code;
+  			$('.debug').append(`<div class='snippet'><div class='snippetName'>${currentItem.name}</div><figure><pre><code class='snippet'${codeSnippet}</code></pre></figure></div>`);
+  			$('.searchSnippets').val('');
+  			found = true;
+  		}
+  	}
+    if (found === false && search) {
+  	  $('.debug').append(`<div class='notFound'>No matching results</div>`);
+    }
+  });
 
+  $('.showAll').on('click', function() {
+  	$('.snippet').remove();
+  	let retrieveData = localStorage.getItem('storedSnippets');
+  	let obj = JSON.parse(retrieveData);
+  	for (var i = 0; i < obj.length; i++) {
+  		let currentItem = obj[i][0];
+  		let codeSnippet = currentItem.code;
+  		let name = currentItem.name;
+  		$('.debug').append(`<div class='snippet'><div class='snippetName'>${name}</div><figure><pre><code class='snippet'>${codeSnippet}</code></pre></figure></div>`);
+  	}
+  });
 
-  $('.textField').on('keyup', function() {
-  	let textFieldValue = $('.textField').val();
-  	$('.debug').text(textFieldValue);
-  })
 
 });
 
+$(document).ready(function(){
+// big data structure
+var snipObj = {
+    data: [],
+};
+    $('.setData').on('click', function(){
+        // When setting data
+        var sendSnippet = function () {
+            var tempObj = {}; // need an object that will store our snippets
+            // Create a tags object later to implement searching by tags.
+            var tempArr = [];
+            let snipName = $('.textName').val();
+            let snippet = $('.textSnip').val();
+            tempObj.name = snipName;
+            tempObj.codeSnippet = snippet;
+            tempArr.push(tempObj);
+            snipObj.data.push(tempArr);
+        }
+        sendSnippet();
+        $('.textSnip').val('');
+        $('.textName').val('');
+        localStorage.setItem('storedSnips', JSON.stringify(snipObj));
+    });
+
+    $('.getSnip').on('click', function(){
+        $('.snippet').remove();
+        $('.noHits').remove();
+        let search = $('.searchInput').val();
+        if (!search) {
+            $('.snippetWrapper').append(`<div class="noHits">You Didn't Search For Anything...</div>`);
+        }
+        let retrieve = localStorage.getItem('storedSnips');
+        let obj = JSON.parse(retrieve); // make data iterable for searching
+        let myCheck = false;
+        for (var i = 0; i < obj.data.length; i++) {
+            let current = obj.data[i][0]; // the actual snippet object created earlier.
+            if (current.name === search) {
+                let snipCode = current.codeSnippet;
+                $('.snippetWrapper').append(`<div class="snippet"><div class="snipName">${current.name}</div><figure><pre><code class="snippet">${snipCode}</code></pre></figure></div>`);
+                $('.searchInput').val('');
+                myCheck = true;
+            }
+        }
+        if (myCheck === false && search) {
+            $('.snippetWrapper').append(`<div class="noHits">No matching snippets...</div>`);
+        }
+    });
+
+    $(".showAll").on('click', function(){
+        $(".snippet").remove();
+        let retrieve = localStorage.getItem('storedSnips');
+        let obj = JSON.parse(retrieve); // make data iterable for searching
+        for (var i = 0; i < obj.data.length; i++) {
+            let current = obj.data[i][0]; // the actual snippet object created earlier.
+            let snipCode = current.codeSnippet;
+            let nameSnip = current.name;
+            $('.snippetWrapper').append(`<div class="snippet"><div class="snipName">${nameSnip}</div><figure><pre><code class="snippet">${snipCode}</code></pre></figure></div>`);
+        }
+    });
+
+});
